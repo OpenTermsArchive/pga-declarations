0fe76d49600e3362266f92386650ae261652760e
afisher3578 <afisher3578@gmail.com>
2022-09-22 16:10:38 +0200

    Create filter to remove download link extension

--- declarations/LinkedIn.filters.js
+++ declarations/LinkedIn.filters.js
@@ -1,15 +1,10 @@
-export function removeHtmlLink(document) {
-  removeQueryParam(document, 'html');
-}
-
-function removeQueryParam(document, queryParam) {
+export function removeDownloadedForUseLinkExtenstion(document) {
   document.querySelectorAll('a').forEach(el => {
-    const href = el.getAttribute('href');
-    const params = new URLSearchParams(href);
+    if (el.innerHTML === 'downloaded for use') {
+      const href = el.getAttribute('href');
+      const cleanedHref = href.replace('.html', '');
 
-    if (params.has(queryParam)) {
-      params.delete(queryParam);
-      el.setAttribute('href', params.toString());
+      el.setAttribute('href', cleanedHref);
     }
   });
 }
