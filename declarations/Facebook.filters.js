f9ad40ff79377a5ab8d9f000af9d3d8ee113c323
afisher3578 <afisher3578@gmail.com>
2022-09-22 09:57:53 +0200

    Create new filter for hrefs

--- declarations/Facebook.filters.js
+++ declarations/Facebook.filters.js
@@ -59,3 +59,8 @@ function removeQueryParam(document, queryParam) {
     }
   });
 }
+
+export function removeLinks(document) {
+  Array.from(document.querySelectorAll('[href]')).map(link =>
+    link.removeAttribute('href'));
+}
