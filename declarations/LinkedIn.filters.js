546fae568de27baf46c7805c389cedf9969423d0
afisher3578 <afisher3578@gmail.com>
2022-09-22 15:22:23 +0200

    Create filter to remove .html in links

--- declarations/LinkedIn.filters.js
+++ declarations/LinkedIn.filters.js
@@ -1,4 +1,15 @@
-export function removeLinks(document) {
-  Array.from(document.querySelectorAll('[href]')).map(link =>
-    link.removeAttribute('href'));
+export function removeHtmlLink(document) {
+  removeQueryParam(document, 'html');
+}
+
+function removeQueryParam(document, queryParam) {
+  document.querySelectorAll('a').forEach(el => {
+    const href = el.getAttribute('href');
+    const params = new URLSearchParams(href);
+
+    if (params.has(queryParam)) {
+      params.delete(queryParam);
+      el.setAttribute('href', params.toString());
+    }
+  });
 }
