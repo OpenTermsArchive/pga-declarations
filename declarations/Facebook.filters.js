9b8521d7f2a7536b8727d095eed22eb1ba5e32aa
afisher3578 <afisher3578@gmail.com>
2022-09-22 15:06:07 +0200

    RemoveTrackingIDs from blinking declarations

--- declarations/Facebook.filters.js
+++ declarations/Facebook.filters.js
@@ -59,8 +59,3 @@ function removeQueryParam(document, queryParam) {
     }
   });
 }
-
-export function removeLinks(document) {
-  Array.from(document.querySelectorAll('[href]')).map(link =>
-    link.removeAttribute('href'));
-}
