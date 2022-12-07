491f44775ad69398f742f82dcf3f532164c8a51b
afisher3578 <afisher3578@gmail.com>
2022-12-07 14:22:06 +0100

    Clean LinkedIn Privacy Policy

--- declarations/LinkedIn.filters.js
+++ declarations/LinkedIn.filters.js
@@ -8,3 +8,20 @@ export function removeDownloadedForUseLinkExtension(document) {
     }
   });
 }
+
+export function removeTrackingIDs(document) {
+  removeQueryParam(document, '?trk=microsites-frontend_legal_privacy-policy&lang=en');
+}
+
+function removeQueryParam(document, queryParam) {
+  document.querySelectorAll('a').forEach(el => {
+    const href = el.getAttribute('href');
+    const params = new URLSearchParams(href);
+
+    if (params.has(queryParam)) {
+      params.delete(queryParam);
+      el.setAttribute('href', params.toString());
+    }
+  });
+}
+
