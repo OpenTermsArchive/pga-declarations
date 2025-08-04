8f78118693595d0616b65f2ea74fa90b93d80ab1
glebbt <gletomas@uni-bremen.de>
2025-08-04 17:19:59 +0200

    Fix X.filters (#457)
    * Fix X.filters
    
    * Fix X.Filters

--- declarations/X.filters.js
+++ declarations/X.filters.js
@@ -5,4 +5,10 @@ export function useXHelpCenterBaseURL(document) {
   document.querySelectorAll(`a[href^="${TwitterHelpCenterBaseURL}"]`).forEach(link => {
     link.href = link.href.replace(TwitterHelpCenterBaseURL, XHelpCenterBaseURL);
   });
+
+  document.querySelectorAll('a[rel*="nofollow"]').forEach(link => {
+    const textNode = document.createTextNode(link.textContent);
+
+    link.parentNode.replaceChild(textNode, link);
+  });
 }
